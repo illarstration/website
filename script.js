b5cfbac24f67a9d42cffced07f7ff5f9ff9f7eaa
Achim Krause <achim.t.krause@gmail.com>
2023-03-19 17:52:28 +0100

    more fixes

--- script.js
+++ script.js
@@ -50,12 +50,12 @@ function init(){
   }
 
   //effective vh hack
-//  let vh = window.innerHeight * 0.01;
-//  document.documentElement.style.setProperty('--vh', `${vh}px`);
-//  window.addEventListener('resize', () => {
-//    let vh = window.innerHeight * 0.01;
-//    document.documentElement.style.setProperty('--vh', `${vh}px`);
-//  });
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+  window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
 }
 
 function spawnPopup(path){
