7c66b6035e57213be9acb7005d703f7296349e3d
Achim Krause <achim.t.krause@gmail.com>
2023-03-19 17:16:56 +0100

    final version with imprint and scroll bug fixed

--- script.js
+++ script.js
@@ -9,8 +9,14 @@ function init(){
     open = 1 - open;
   }
   
-  document.querySelector(".header>img.logo").addEventListener('click',(e)=>{
+  document.querySelector(".header>img.logo.narrow").addEventListener('click',(e)=>{
     toggleOpen();
+    e.stopPropagation();
+  });
+  document.addEventListener('click',(e)=>{
+    if(open){
+      toggleOpen();
+    }
   });
       
   document.addEventListener('touchstart', e => {
@@ -26,29 +32,31 @@ function init(){
 
   //init popup
   let popup = document.getElementById('popup');
-  console.log(popup);
-  popup.addEventListener('click', () => {
-    popup.style.display = 'none';
-  });
-  let imgs = document.querySelectorAll('.pictureframe > img');
-  console.log(imgs);
-  imgs.forEach(img => {
-    console.log(img.src);
-    let name = img.src.split('_').pop();
-    let path = "img/g-cont_" + name;
-    console.log(path);
-    img.addEventListener('click', () => {
-       spawnPopup(path);
+  if(popup !== null){
+    console.log(popup);
+    popup.addEventListener('click', () => {
+      popup.style.display = 'none';
     });
-  });
+    let imgs = document.querySelectorAll('.pictureframe > img');
+    console.log(imgs);
+    imgs.forEach(img => {
+      console.log(img.src);
+      let name = img.src.split('_').pop();
+      let path = "img/g-cont_" + name;
+      console.log(path);
+      img.addEventListener('click', () => {
+         spawnPopup(path);
+      });
+    });
+  }
 
   //effective vh hack
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-  window.addEventListener('resize', () => {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  });
+//  let vh = window.innerHeight * 0.01;
+//  document.documentElement.style.setProperty('--vh', `${vh}px`);
+//  window.addEventListener('resize', () => {
+//    let vh = window.innerHeight * 0.01;
+//    document.documentElement.style.setProperty('--vh', `${vh}px`);
+//  });
 }
 
 function spawnPopup(path){
